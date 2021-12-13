0fc1d559a0b3cb7fe11922b1345f01e50fc9af03
tommy <tommy5566123@gmail.com>
2021-12-13 19:19:27 +0800

    Initial Commit

---
+++ FFMI_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FFMI_App
+//
+//  Created by 蔡尚諺 on 2021/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
